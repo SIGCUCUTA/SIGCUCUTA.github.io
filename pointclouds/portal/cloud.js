f7576e316e0588ba5861025b57b4d186637b2f56
SIGCUCUTA <goodwillsigcucuta@gmail.com>
2024-02-19 15:52:53 -0500

    GOOD WILL SIG
    PAGINA WEB GOOD WILL SIG

---
+++ pointclouds/portal/cloud.js
@@ -0,0 +1,26 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=utm +zone=10 +datum=NAD83 +units=m +no_defs",
+    "points": 578590,
+    "boundingBox": {
+        "lx": 522945.88,
+        "ly": 4624691.39,
+        "lz": 823.19,
+        "ux": 523217.53,
+        "uy": 4624963.04,
+        "uz": 1094.8400000000233
+    },
+    "tightBoundingBox": {
+        "lx": 522945.88,
+        "ly": 4624691.39,
+        "lz": 823.19,
+        "ux": 523217.53,
+        "uy": 4624877.03,
+        "uz": 894.36
+    },
+    "pointAttributes": "LAZ",
+    "spacing": 2.352557897567749,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
